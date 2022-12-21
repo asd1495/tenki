ada6761ac2a72bb3bfe636c0fb1793d8000f6af3
asd1495 <armando.salcedo95@gmail.com>
2022-12-20 23:43:42 -0600

    configured react

---
+++ tenki/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Search from './components/Search/Search';
+
+function App() {
+  return (
+    <div className="container">
+      <Search />
+    </div>
+  );
+}
+
+export default App;
