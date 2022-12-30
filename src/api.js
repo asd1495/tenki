67df6c0782adc71d307340b6ff8846fd1c3e8721
asd1495 <armando.salcedo95@gmail.com>
2022-12-29 23:21:01 -0600

    added search function

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c09c0f4c67msh69db3d36f6986abp150d04jsn7ca145836b00',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
